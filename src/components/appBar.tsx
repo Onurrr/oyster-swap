99f87b4c4d2f2cc9fc946fbf0a9c2cff87f5750a
Onur <0896269@hr.nl>
2021-01-12 02:28:44 +0100

    menu fix

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -10,93 +10,95 @@ export const AppBar = (props: { left?: JSX.Element; right?: JSX.Element }) => {
   const history = useHistory();
 
   const TopBar = (
-    <div className="App-Bar">
-      <div className="App-Bar-left">
-        <div className="App-logo" />
-        <Menu mode="horizontal" selectedKeys={[location.pathname]}>
-          <Menu.Item key="/">
-            <Link
-              to={{
-                pathname: "/",
-              }}
-            >
-              Swap
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/add">
-            <Link
-              to={{
-                pathname: "/add",
-              }}
-            >
-              Pool
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/info">
-            <Link
-              to={{
-                pathname: "/info",
-              }}
-            >
-              Charts
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="trade">
-            <a
-              href={"https://dex.projectserum.com"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Trade
-              <sup>↗</sup>
-            </a>
-          </Menu.Item>
-          <Menu.Item key="help">
-            <a
-              href={"https://serum-academy.com/en/serum-swap/"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Help
-              <sup>↗</sup>
-            </a>
-          </Menu.Item>
-        </Menu>
-        {props.left}
-      </div>
-      <div className="App-Bar-right">
-        <AccountInfo />
-        {connected && (
-          <Button
-            type="text"
-            size="large"
-            onClick={() => history.push({ pathname: "/pool" })}
-          >
-            My Pools
-          </Button>
-        )}
-        <div>
-          {!connected && (
-            <Button
-              type="text"
-              size="large"
-              onClick={connected ? wallet.disconnect : wallet.connect}
-              style={{ color: "#2abdd2" }}
-            >
-              Connect
-            </Button>
-          )}
-          {connected && (
-            <Popover
-              placement="bottomRight"
-              title="Wallet public key"
-              trigger="click"
-            ></Popover>
-          )}
+  <div className="bg-indigo-600 pb-32">
+    <nav className="bg-indigo-600 border-b border-indigo-300 border-opacity-25 lg:border-none">
+      <div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+        <div className="relative h-16 flex items-center justify-between lg:border-b lg:border-indigo-400 lg:border-opacity-25">
+          <div className="px-2 flex items-center lg:px-0">
+            <div className="flex-shrink-0">
+              <img className="block h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg" alt="Workflow"/>
+            </div>
+            <div className="hidden lg:block lg:ml-10">
+              <div className="flex space-x-4">
+
+                <a href="#" className="bg-indigo-700 text-white rounded-md py-2 px-3 text-sm font-medium">
+                  Dashboard
+                </a>
+
+                <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 rounded-md py-2 px-3 text-sm font-medium">
+                  Team
+                </a>
+
+                <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 rounded-md py-2 px-3 text-sm font-medium">
+                  Projects
+                </a>
+
+                <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 rounded-md py-2 px-3 text-sm font-medium">
+                  Calendar
+                </a>
+
+                <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 rounded-md py-2 px-3 text-sm font-medium">
+                  Reports
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex lg:hidden">
+            <button className="bg-indigo-600 p-2 rounded-md inline-flex items-center justify-center text-indigo-200 hover:text-white hover:bg-indigo-500 hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+              <svg className="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+          <div className="hidden lg:block lg:ml-4">
+            <div className="flex items-center">
+              <button className="bg-indigo-600 flex-shrink-0 rounded-full p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white">
+                <span className="sr-only">View notifications</span>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                  </svg>
+              </button>
+            </div>
+          </div>
         </div>
-        {props.right}
       </div>
-    </div>
+
+      <div className="hidden lg:hidden">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <a href="#" className="bg-indigo-700 text-white block rounded-md py-2 px-3 text-base font-medium">
+            Dashboard
+          </a>
+
+          <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 block rounded-md py-2 px-3 text-base font-medium">
+            Team
+          </a>
+
+          <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 block rounded-md py-2 px-3 text-base font-medium">
+            Projects
+          </a>
+
+          <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 block rounded-md py-2 px-3 text-base font-medium">
+            Calendar
+          </a>
+
+          <a href="#" className="text-white hover:bg-indigo-500 hover:bg-opacity-75 block rounded-md py-2 px-3 text-base font-medium">
+            Reports
+          </a>
+        </div>
+      </div>
+    </nav>
+    <header className="py-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-white">
+          Dashboard
+        </h1>
+      </div>
+    </header>
+  </div>
   );
 
   return TopBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const fontFamily = defaultTheme.fontFamily;
+fontFamily['sans'] = [
+  'Inter var',
+  'system-ui',
+];
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: fontFamily,
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
